0d11ee0a3bfa94671e6d865fde41680ab92c0a64
ksarathkumarreddy <sharathkumarreddy24@gmail.com>
2020-02-28 20:42:09 +0530

    tuple commited

---
+++ GitProjectv1/src/sample.py
@@ -0,0 +1,8 @@
+'''
+Created on Feb 28, 2020
+
+@author: Pavan
+'''
+print("hello vishnu this is sarath")
+
+print("may all ur dreams come true")
---
+++ GitProjectv1/src/test.py
@@ -0,0 +1,8 @@
+'''
+Created on Feb 28, 2020
+
+@author: Pavan
+'''
+list=[1,2,3]
+print(list)
+print(id(list))
